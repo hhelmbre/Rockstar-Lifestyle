e77dcc78442af3da6580cb7b241ca84a6d3eb021
Hawley Helmbrecht <hhelmbre@uw.edu>
2019-03-04 15:19:38 -0800

    Finished the fourier transform function and a function to show the Magnitude spectrum

--- examples/fouriertransform.py
+++ examples/fouriertransform.py
@@ -1,9 +1,13 @@
 #Purpose: To perform the fourier transform
 
 #import necessary packages
-import import numpy as np
+import numpy as np
+from matplotlib import pyplot as plt
 
 #Function 1: Performs the fourier transform with steps to get Magnitude_Spectrum
+#Steps: fourier transform, fourier shift, gets the magnitude Spectrum
+#Inputs: images
+#Outputs: magntidue spectrum array
 def Magnitude_Spectrum(image):
     f = np.fft.fft2(image)
     fshift= np.fft.fftshift(f)
@@ -11,8 +15,11 @@ def Magnitude_Spectrum(image):
     return m_spec
 
 #Function 2: Plotting the magnitude spectrum made my fourier transformM
+#Steps: plots the Magnitude Spectrum
+#Input: the magnitude spectrum Output
+#Output: none it just shows the image for funsies
 def Plot_M_Spec(m_spec):
-        plt.imshow(m_spec, cmap = 'gray')
-        plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-        plt.show()
-        return
+    plt.imshow(m_spec, cmap = 'gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.show()
+    return
