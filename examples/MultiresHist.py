5bc34b9ded75537a5839ba1d82c4db1507e5ffe9
Hawley Helmbrecht <hhelmbre@uw.edu>
2019-03-04 10:38:34 -0800

    Added a function for performing the guassian blurring

---
+++ examples/MultiresHist.py
@@ -0,0 +1,61 @@
+#Purpose: To hold functions used for the multiresolution histogram
+
+#Importing necessary packages
+from PIL import Image, ImageFilter
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy
+from scipy.ndimage import gaussian_filter
+import MultiresHist as mult
+import timeit
+
+#Function 1: Importing images
+def image_import():
+    pass
+
+#Function 2: Prepping the images
+## Splits the image by color
+##
+def image_prep():
+    pass
+
+#Function 3: Gaussian Filter Application
+gauss_blurs = []
+def gauss_filter(image, gauss_blur_list):
+    for levels in gauss_blur_list:
+        gauss_blur = gaussian_filter(image, sigma=levels)
+        gauss_blurs.append(gauss_blur)
+    return gauss_blurs
+
+#Function 4: Function for creating empty lists for each gaussian blur applied
+
+
+#Function 5: Obtaining Histograms
+#need to have variable bins functionality as well
+hist = []
+def cumulative_hist(gauss_blur_list, bin_list):
+    for bin_count in bin_list:
+        fig = plt.figure()
+        hist_itt = plt.hist(gauss_blur_list, bins=bin_count, density = True, cumulative = True)
+        hist.append(hist_itt)
+    return hist
+
+#Function 6: Obtaining difference histograms between two cumulative histograms
+heights = []
+def diff_hist(hist1_plot, hist2_plot):
+    bins1 = hist1_plot[0]
+    bins2 = hist2_plot[0]
+    bins = len(bins1)
+
+    for i in range(bins):
+        array1 = bins1[i]
+        array2 = bins2[i]
+        array_length = len(array1)
+        heights_bins = []
+        for j in range(array_length):
+            bin1 = array1[j]
+            bin2 = array2[j]
+            height = bin1 - bin2
+            heights_bins.append(height)
+            heights.append(heights_bins)
+    return heights
