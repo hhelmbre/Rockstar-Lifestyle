56eea0bd65110ceccd33d3628ab3867ab674e2e2
Hawley Helmbrecht <hhelmbre@uw.edu>
2019-03-04 15:32:03 -0800

    Added the function for changing image contrast

--- examples/preprocessing.py
+++ examples/preprocessing.py
@@ -1,2 +1,16 @@
 #Purpose: For functions that perform pre-processessing of images
+from PIL import Image, ImageFilter, ImageEnhance
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy
 
+def color_split():
+    pass
+
+#Function X: To change image contrast
+def im_contrast(image, contrast_value)
+    img = Image.fromarray(image)
+    img = img.convert("L")
+    enhancer = ImageEnhance.Contrast(img)
+    enhanced_im = enhancer.enhance(contrast_value)
+    return enhanced_im
