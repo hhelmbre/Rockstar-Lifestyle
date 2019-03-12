ab3f2ba2224ed9acd2d98702b1d80b5baddb6a89
Sage Scheiwiller <sschei@uw.edu>
2019-03-11 21:14:56 -0700

    Corrected the test files - all running with asserts - inputs/outputs and some other output checks

--- examples/Image_training_pixels.py
+++ examples/Image_training_pixels.py
@@ -5,6 +5,10 @@ from random import randint
 #from function files
 import Image_training_pixels
 
+#Function 1: Generates a inputed number of images with random white pixels on a black background
+#Steps: set seed, create a loop through requested number of images creating the Pixels on the background
+#Inputs: requested number of images
+#Outputs: an array that has the images saved as numpy arrays
 def random_image_generator(n):
     """Generates a number of images with randomized white pixels on a black
 background"""
@@ -29,7 +33,10 @@ background"""
         imgarray.append(array) #adds new array to the list of arrays
     return imgarray
 
-
+#Function 2: Counts the number of white pixels in the image and records that
+#Steps: Sets the counters for the black and obj pixels, runs through each pixel in the image and determines if it is black or white and then records the count of said color
+#Inputs: the image array produced in the image generatior and the index the arrays
+#Outputs: the number of black pixels and how many objects or white pixel
 def pixel_counter_single_image(array, array_index):
     """Counts the number of colored pixels for a single image"""
     black = 0 #sets the counter to 0
@@ -45,7 +52,10 @@ def pixel_counter_single_image(array, array_index):
     return black, obj
 
 
-
+#Function 3: Runs the previous pixel counter for all requested images
+#Steps: runs through a for loop of the number of images that were intially generated and runs the pixel counter for single images for each one.
+#Inputs: the image array generated in the image_generator_rectangles
+#Outputs: pixel count which contains the index, the numbers of objects normalized for each one, the number of black pixels and the total pixels.
 def pixel_counter_whole_array(array):
     """Counts the number of pixels for """
     i = 0
@@ -53,7 +63,6 @@ def pixel_counter_whole_array(array):
     for i in range(0,len(array)): #counts through the 'images' in the given array
         array_index = i
         black, obj = Image_training_pixels.pixel_counter_single_image(array, array_index)
-        total = black + obj #sums up the number of pixels counted. Images
-			    #are 250x250
+        total = black + obj #sums up the number of pixels counted. Image are 250x250 so total should be 62500
         pixel_count.loc[i] = [i, obj, black, total]
     return pixel_count
