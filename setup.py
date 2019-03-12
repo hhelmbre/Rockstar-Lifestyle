db613ad2eeaea1a0c408087bec77f533928da84a
Sage Scheiwiller <sschei@uw.edu>
2019-03-12 10:20:48 -0700

    Created setup.py file and put the basic stuff i understood in there. Renamed our file - will import as RockstarLifestyle

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'Rockstar_Lifestyle'
+    version = '1.0.0'
+    packages = ['RockstarLifestyle',]
+    license = 'MIT'
+    long_description = open('README.MD').read()
+)
