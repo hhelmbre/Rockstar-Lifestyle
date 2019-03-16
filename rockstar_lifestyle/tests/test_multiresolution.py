c8489003e60051eaff8aad76a948a95d07df10a3
Sage Scheiwiller <sschei@uw.edu>
2019-03-15 18:04:44 -0700

    edits for pep8 and naming

--- rockstar_lifestyle/tests/test_multiresolution.py
+++ rockstar_lifestyle/tests/test_multiresolution.py
@@ -16,68 +16,71 @@ from rockstar_lifestyle import multiresolution, preprocessing
 
 #Function 3: Test of Gaussian Filter Application
 def test_gauss_filter():
-#inputs for functions
+    """Test: Applies a Guassian Blur to all levels in list"""s
+    #inputs for functions
     image = Image.open('Test_Photo_fromMike.png')
     desired_color = 'g'
     image = preprocessing.color_split_image(image, desired_color)
     gauss_blur_list = [0,1] #function currently only works with two gauss blurs
-#function running
+    #function running
     gauss_blurs = multiresolution.gauss_filter(image, gauss_blur_list)
-#asserts and checks
+    #asserts and checks
     assert isinstance(desired_color, str), "intial input is wrong"
     assert isinstance(gauss_blur_list, list), "input is wrong"
     return gauss_blurs
 
 #Function 4: Test of Obtaining histograms
 def test_cumulative_hist():
-#inputs for function
+    """Test: Creates a cumulative histogram through list"""
+    #inputs for function
     image = Image.open('Test_Photo_fromMike.png')
     desired_color = 'g'
     image = preprocessing.color_split_image(image, desired_color)
     gauss_blur_list = [0,1] #function currently only works with two gauss blurs
     gauss_blur_images = multiresolution.gauss_filter(image, gauss_blur_list)
     bin_list = [3] #number of bins desired for histogram
-#function running
+    #function running
     hist = multiresolution.cumulative_hist(gauss_blur_images, bin_list)
-#asserts and checks
+    #asserts and checks
     assert isinstance(gauss_blur_list, list), "input is wrong"
-
     return hist
 
 #Function 5: Test for determining plot 1 and plot 2
 def test_diff_plot_determination():
     """Test: Creates cumlative histograms and splits them into plot 1 and 2"""
-#inputs for function
+    #inputs for function
     image = Image.open('Test_Photo_fromMike.png')
     desired_color = 'g'
-    gauss_blur_list = [0,1] #function currently only works with two gauss blurs
-    bin_list = [3] #number of bins desired for histogram
-#running the functions
+    #function currently only works with two gauss blurs
+    gauss_blur_list = [0,1]
+    bin_list = [3]
+    #running the functions
     hist1_plot, hist2_plot = multiresolution.diff_plot_determination(
         image, desired_color, gauss_blur_list, bin_list)
-#asserts and checks
+    #asserts and checks
     return hist1_plot, hist2_plot
 
 #Function 6: Test of obtianing difference histograms between two cumulative histograms
 def test_diff_hist():
     """Test: Determines differences between two cumulative histogram"""
-#inputs for functions
+    #inputs for functions
     image = Image.open('Test_Photo_fromMike.png')
     desired_color = 'g'
     gauss_blur_list = [0,1] #function currently only works with two gauss blurs
     bin_list = [3] #number of bins desired for histogram
-# getting the two plots needed to run difference
+    #getting the two plots needed to run difference
     hist1_plot, hist2_plot = multiresolution.diff_plot_determination(
         image, desired_color, gauss_blur_list, bin_list)
-#function running
+        #function running
     heights = multiresolution.diff_hist(hist1_plot, hist2_plot)
-# asserts and checks
+    #asserts and checks
     return heights
 
 #Function 7: Test for concatenating difference Histograms
 
 #Function 8: Test for plotting the full multi res histograms
 # def test_Multi_res_hist_full():
+#     """Wraps all functions and produces a plot of height comparisons"""
 # #inputs for functions
 #     image = Image.open('Test_Photo_fromMike.png')
 #     desired_color = 'g'
