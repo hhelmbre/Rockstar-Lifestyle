8a38010d1604dd356dbb6453e0b36eef4c9617e4
Hawley Helmbrecht <hhelmbre@uw.edu>
2019-03-19 12:37:58 -0700

    I delted some unnecessary import files and I fixed it so that it is workign on pip install :)

--- rockstarlifestyle/objects.py
+++ rockstarlifestyle/objects.py
@@ -2,17 +2,12 @@
 
 #Import necessary packages
 import numpy as np
-import rockstar_lifestyle
 import scipy
-from scipy.ndimage import gaussian_filter
-from skimage import filters
-from skimage.morphology import watershed
+#from scipy.ndimage import gaussian_filter
+#from skimage.morphology import watershed
 from skimage.feature import peak_local_max
-from skimage import measure
-from skimage import morphology
+from skimage import measure, morphology, segmentation, data, feature, filters
 from scipy import ndimage
-from skimage import segmentation
-from skimage import data, feature
 import math
 import matplotlib.pyplot as plt
 
