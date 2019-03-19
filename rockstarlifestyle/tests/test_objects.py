e286c18c5c8abe341f506e8ddd1440cb00c2529b
Sage Scheiwiller <sschei@uw.edu>
2019-03-19 14:23:30 -0700

    modifed tests - did not modify object stats

--- rockstarlifestyle/tests/test_objects.py
+++ rockstarlifestyle/tests/test_objects.py
@@ -1,147 +1,183 @@
 #Purpose: To Obtain object stats from the python images
 
 #Import necessary packages
-import numpy as np
-import rockstar_lifestyle
-import scipy
 from rockstarlifestyle import objects, preprocessing
-
-from PIL import Image, ImageFilter, ImageEnhance
-from scipy.ndimage import gaussian_filter
-from skimage import filters
-from skimage.morphology import watershed
+import numpy as np
+import scipy
 from skimage.feature import peak_local_max
-from skimage import measure
-from skimage import morphology
+from skimage import measure, morphology, segmentation, data, feature, filters
 from scipy import ndimage
-from skimage import segmentation
-from skimage import data, feature
 import math
 import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter, ImageEnhance
 
 
-def test_global_binary():
-    #inputs for functions
-    image = Image.open('Test_Photo_fromMike.png')
-    #running the functions
-    global_binary = objects.global_binary(image)
-    #asserts and checks
-    return
-
-def test_local_binary():
-    #inputs for functions
-    image = Image.open('Test_Photo_fromMike.png')
-    desired_color = 'b'
-    #processing
-    split = preprocessing.color_split_image(Image, desired_color)
-    image = preprocessing.image_contrast(split, 2)
-    block_size = 15
-    #running the functions
-    binary_adaptive = objects.local_binary(image, block_size)
-    #asserts and checks
-    return
-
-def test_global_labels():
-    #inputs for functions
-    image = Image.open('Test_Photo_fromMike.png')
-    desired_color = 'b'
-    #processing
-    split = preprocessing.color_split_image(Image, desired_color)
-    image = preprocessing.image_contrast(split, 2)
-    #running the functions
-    otsu_global_labels = objects.global_labels(image)
-    #asserts and checks
-    return
-
-def test_local_labels():
-    #inputs for functions
-    image = Image.open('Test_Photo_fromMike.png')
-    desired_color = 'b'
-    #processing
-    split = preprocessing.color_split_image(Image, desired_color)
-    image = preprocessing.image_contrast(split, 2)
-    block_size = 15
-    #running the functions
-    local_adaptive_labels = objects.local_labels(image, block_size)
-    #asserts and checks
-    return
-
-def test_object_area_hist():
-    #inputs for functions
-    image = Image.open('Test_Photo_fromMike.png')
-    desired_color = 'b'
-    #processing
-    split = preprocessing.color_split_image(Image, desired_color)
-    image = preprocessing.image_contrast(split, 2)
-    block_size = 15
-    local_adaptive_labels = objects.local_labels(image, block_size)
-    otsu_global_labels = objects.global_labels(image)
-    #final inputs
-    properties_local = measure.regionprops(local_adaptive_labels)
-    properties_global = measure.regionprops(otsu_global_labels)
-    #running the functions
-    objects.object_area_hist(properties_local, properties_global)
-    #asserts and checks
-    return
-
-
-def test_centroid_distance():
-    #inputs for functions
-    #running the functions
-    otsu_global_labels = global_labels(image)
-    properties_global = measure.regionprops(otsu_global_labels)
-    image_centroid = properties_global[0].centroid
-    distance = objects.centroid_distance(image_centroid, object_centroid, row)
-    #asserts and checks
-    return
+# def test_global_binary():
+#     """Test: obtains the binary using the Global Otsu Threshold """
+#     #inputs for functions
+#     image = Image.open('../data/protein_matrix_image.png')
+#     #running the functions
+#     global_binary = objects.global_binary(image)
+#     #asserts and checks
+#     return
+#
+# def test_local_binary():
+#     """Test: obtains the binary of the image using a local adaptive threshold"""
+#     #inputs for functions
+#     image = Image.open('../data/protein_matrix_image.png')
+#     desired_color = 'b'
+#     #processing
+#     split = preprocessing.color_split_image(image, desired_color)
+#     image = preprocessing.image_contrast(split, 2)
+#     block_size = 15
+#     #running the functions
+#     binary_adaptive = objects.local_binary(image, block_size)
+#     #asserts and checks
+#     return
+#
+# def test_global_labels():
+#     """Test: obtains the labels for objects using the global otsu threshold"""
+#     #inputs for functions
+#     image = Image.open('../data/protein_matrix_image.png')
+#     desired_color = 'b'
+#     #processing
+#     split = preprocessing.color_split_image(image, desired_color)
+#     image = preprocessing.image_contrast(split, 2)
+#     #running the functions
+#     otsu_global_labels = objects.global_labels(image)
+#     #asserts and checks
+#     return
+#
+# def test_local_labels():
+#     """Test: obtains the lables using a local adaptive threshold for objects"""
+#     #inputs for functions
+#     image = Image.open('../data/protein_matrix_image.png')
+#     desired_color = 'b'
+#     #processing
+#     split = preprocessing.color_split_image(image, desired_color)
+#     image = preprocessing.image_contrast(split, 2)
+#     block_size = 15
+#     #running the functions
+#     local_adaptive_labels = objects.local_labels(image, block_size)
+#     #asserts and checks
+#     return
+#
+# def test_object_area_hist():
+#     """Test: makes 10-bin histograms of the object areas"""
+#     #inputs for functions
+#     image = Image.open('../data/protein_matrix_image.png')
+#     desired_color = 'b'
+#     #processing
+#     split = preprocessing.color_split_image(image, desired_color)
+#     image = preprocessing.image_contrast(split, 2)
+#     block_size = 15
+#     local_adaptive_labels = objects.local_labels(image, block_size)
+#     otsu_global_labels = objects.global_labels(image)
+#     #final inputs
+#     properties_local = measure.regionprops(local_adaptive_labels)
+#     properties_global = measure.regionprops(otsu_global_labels)
+#     #running the functions
+#     objects.object_area_hist(properties_local, properties_global)
+#     #asserts and checks
+#     return
+# #
+#
+# def test_centroid_distance():
+#     """Test: computes distance between an object centroid and image centroid"""
+#     #inputs for functions
+#     image = Image.open('../data/protein_matrix_image.png')
+#     desired_color = 'b'
+#     #processing
+#     split = preprocessing.color_split_image(image, desired_color)
+#     image = preprocessing.image_contrast(split, 2)
+#     block_size = 15
+#     otsu_global_labels = objects.global_labels(image)
+#     properties_global = measure.regionprops(otsu_global_labels)
+#     image_centroid = properties_global[0].centroid
+#     #running the functions
+#     distance = objects.centroid_distance(image_centroid, object_centroid, row)
+#     #asserts and checks
+#     return
 
 def test_distancesarr():
+    """Test: gets the distances between image and objects"""
     #inputs for functions
+    image = Image.open('../data/protein_matrix_image.png')
+    desired_color = 'b'
+    #processing
+    split = preprocessing.color_split_image(image, desired_color)
+    image = preprocessing.image_contrast(split, 2)
+    block_size = 15
+    #final inputs
+    otsu_global_labels = objects.global_labels(image)
+    properties_global = measure.regionprops(otsu_global_labels)
+    image_centroid = properties_global[0].centroid
+    global_binarys = objects.global_binary(image)
+    object_centroids = feature.blob_log(global_binarys)
+    row = 3
     #running the functions
+    distances = objects.distancesarr(image_centroid, object_centroids)
     #asserts and checks
     return
 
-def test_objectcentroids():
-    #inputs for functions
-    image = Image.open('Test_Photo_fromMike.png')
-    desired_color = 'b'
-    #processing
-    split = preprocessing.color_split_image(Image, desired_color)
-    contrast_image = preprocessing.image_contrast(split, 2)
-    block_size = 15
-    #running the functions
-    object_centroids, object_centroids_local = objects.objectcentroids(contrast_image, block_size)
-    #asserts and checks
-    return
-
-def test_distance_histograms():
-    #inputs for functions
-    image = Image.open('Test_Photo_fromMike.png')
-    desired_color = 'b'
-    #processing
-    split = preprocessing.color_split_image(Image, desired_color)
-    contrast_image = preprocessing.image_contrast(split, 2)
-    block_size = 15
-    #running the functions
-    objects.distance_histograms(contrast_image, block_size)
-    #asserts and checks
-    return
-
-def test_objectnumber():
-    #inputs for functions
-    image = Image.open('Test_Photo_fromMike.png')
-    desired_color = 'b'
-    #processing
-    split = preprocessing.color_split_image(Image, desired_color)
-    contrast_image = preprocessing.image_contrast(split, 2)
-    block_size = 15
-    #running the functions
-    objects.objectnumber(contrast_image, block_size)
-    #asserts and checks
-    return
+# def test_objectcentroids():
+#     #inputs for functions
+#     image = Image.open('../data/protein_matrix_image.png')
+#     desired_color = 'b'
+#     #processing
+#     split = preprocessing.color_split_image(image, desired_color)
+#     contrast_image = preprocessing.image_contrast(split, 2)
+#     block_size = 15
+#     #running the functions
+#     object_centroids, object_centroids_local = objects.objectcentroids(contrast_image, block_size)
+#     #asserts and checks
+#     return
+#
+# def test_distance_histograms():
+#     #inputs for functions
+#     image = Image.open('../data/protein_matrix_image.png')
+#     desired_color = 'b'
+#     #processing
+#     split = preprocessing.color_split_image(image, desired_color)
+#     contrast_image = preprocessing.image_contrast(split, 2)
+#     block_size = 15
+#     #running the functions
+#     objects.distance_histograms(contrast_image, block_size)
+#     #asserts and checks
+#     return
+#
+# def test_objectnumber():
+#     #inputs for functions
+#     image = Image.open('../data/protein_matrix_image.png')
+#     desired_color = 'b'
+#     #processing
+#     split = preprocessing.color_split_image(image, desired_color)
+#     contrast_image = preprocessing.image_contrast(split, 2)
+#     block_size = 15
+#     #running the functions
+#     objects.objectnumber(contrast_image, block_size)
+#     #asserts and checks
+#     return
 
 def test_stats():
+    """Test: gets basic stats for whatever property input"""
     #inputs for functions
+    image = Image.open('../data/protein_matrix_image.png')
+    desired_color = 'b'
+    #processing
+    split = preprocessing.color_split_image(image, desired_color)
+    image = preprocessing.image_contrast(split, 2)#inputs for functions
+    #labels
+    otsu_global_labels = objects.global_labels(image)
+    properties_global = measure.regionprops(otsu_global_labels)
+    image_centroid = properties_global[0].centroid
+    global_binarys = objects.global_binary(image)
+    object_centroids = feature.blob_log(global_binarys)
+    row = 3
+    distance = objects.centroid_distance(image_centroid, object_centroids, row)    #running the functions
+    #final inputs
+    property = distance
     #running the functions
+    objects.stats(property)
     #asserts and checks
     return
